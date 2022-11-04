d61565ae6d620af1032122f0b87e8576f23a5523
kalvinbenser <kalvinbenser1@gmail.com>
2022-11-04 18:20:37 +0530

    add your self error alert fix

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ db.sequelize
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Pethowz server is running" });
 });
 
 // //router
